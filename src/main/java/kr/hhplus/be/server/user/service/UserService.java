171c903ab05aa28e48613a1e5ab3b82d0b351a27
SMJin <sstaw@naver.com>
2025-06-06 20:44:42 +0900

    feature: 비밀번호 암호화 및 Sprig Security 도입 #16

--- src/main/java/kr/hhplus/be/server/user/service/UserService.java
+++ src/main/java/kr/hhplus/be/server/user/service/UserService.java
@@ -4,6 +4,7 @@ import kr.hhplus.be.server.user.dto.SignUpRequest;
 import kr.hhplus.be.server.user.entity.User;
 import kr.hhplus.be.server.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,15 +12,18 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User register(SignUpRequest request) {
         if (userRepository.existsByUsername(request.getUsername())) {
             throw new IllegalArgumentException("이미 존재하는 사용자입니다.");
         }
 
+        String hashedPassword = passwordEncoder.encode(request.getPassword());
+
         User user = User.builder()
                 .username(request.getUsername())
-                .password(request.getPassword())
+                .password(hashedPassword)
                 .role("ROLE_USER")
                 .build();
 
